dbd1152fdc0c45c962719e911458d1e43fca03c1
Stan Li <stanlyeeee@gmail.com>
2026-05-16 20:27:24 +0100

    Implement part 1 forward kinetics

--- viewer/student_submission/part1_fk.py
+++ viewer/student_submission/part1_fk.py
@@ -21,10 +21,31 @@ def forward_kinematics(
     - world_rotations: shape (J, 3, 3)
     - world_positions: shape (J, 3)
     """
+    # joint_count = len(joints)
+    # world_rotations = np.tile(
+    #     np.eye(3, dtype=np.float32)[None, :, :],
+    #     (joint_count, 1, 1),
+    # )
+    # world_positions = np.zeros((joint_count, 3), dtype=np.float32)
     joint_count = len(joints)
-    world_rotations = np.tile(
-        np.eye(3, dtype=np.float32)[None, :, :],
-        (joint_count, 1, 1),
-    )
-    world_positions = np.zeros((joint_count, 3), dtype=np.float32)
+    world_rotations = np.zeros((joint_count, 3, 3))
+    world_positions = np.zeros((joint_count, 3))
+    
+    if topological_order is None:
+        iteration_order = range(joint_count)
+    else:
+        iteration_order = topological_order
+
+    for index in iteration_order:
+        joint_parent = joints[index].parent
+        if joint_parent < 0:
+            world_positions[index] = joints[index].translation + root_offset
+            world_rotations[index] = local_rotations[index]
+        else:
+            world_rotations[index] = world_rotations[joint_parent] @ local_rotations[index]
+            world_positions[index] = world_positions[joint_parent] + world_rotations[joint_parent] @ joints[index].translation
+
+
     return world_rotations, world_positions
+
+
